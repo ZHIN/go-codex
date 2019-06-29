69b8c30cc8ffe04d65c286bb2d9230b401b2d7f5
zhin <n2iyp2lf>
2019-06-30 01:10:22 +0800

    change database

--- database/curd.go
+++ database/curd.go
@@ -2,93 +2,71 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
+	"math"
 	"reflect"
+	"strings"
 
 	"github.com/jinzhu/gorm"
-	"github.com/sirupsen/logrus"
-	"github.com/zhin/go-codex/cerror"
 )
 
-type DatabaseRepo struct {
-	dbKey   string
-	channel chan int
-}
+const getDBObjectErrorMessage = "无法获取数据库对象"
 
-func (r *DatabaseRepo) put() {
-	r.channel <- 0
-}
-
-func (r *DatabaseRepo) pop() {
-	<-r.channel
-}
+// Create 创建数据
 func (r *DatabaseRepo) Create(item interface{}) error {
-
 	r.put()
 	defer r.pop()
+	db, err := getDB(r.dbKey)
 
-	db, err := GetDB(r.dbKey)
 	if err != nil {
-		return cerror.NewCodeError(cerror.DB_ERROR, err)
+		return err
 	}
 	defer db.Close()
-	if err := db.Create(item).Error; err != nil {
-		logrus.Warnf("DB.Create Error Conn:%s Type:%s (%s)", r.dbKey, reflect.TypeOf(item).String(), err.Error())
-		return cerror.NewCodeError(cerror.DB_ERROR, err)
+
+	db = db.Create(item)
+	if err := db.Error; err != nil {
+		return warpDBError(db, "DB.Create", fmt.Sprintf("DB.Create Error Conn:%s Type:%s", r.dbKey, reflect.TypeOf(item).String()))
 	}
 	return nil
 }
 
+// Save 保存数据
 func (r *DatabaseRepo) Save(item interface{}) error {
 
 	r.put()
 	defer r.pop()
 
-	db, err := GetDB(r.dbKey)
+	db, err := getDB(r.dbKey)
 	if err != nil {
-		return cerror.NewCodeError(cerror.DB_ERROR, err)
+		return err
 	}
 	defer db.Close()
-	if err := db.Save(item).Error; err != nil {
-		logrus.Warnf("DB.Save Error Conn:%s Type:%s (%s)", r.dbKey, reflect.TypeOf(item).String(), err.Error())
-		return cerror.NewCodeError(cerror.DB_ERROR, err)
+	db = db.Save(item)
+	if err := db.Error; err != nil {
+		return warpDBError(db, "DB.Save", fmt.Sprintf("DB.Save Error Conn:%s Type:%s", r.dbKey, reflect.TypeOf(item).String()))
 	}
 	return nil
 }
 
-func (r *DatabaseRepo) SaveMany(items ...interface{}) error {
-
-	return r.InvokeTransation(func(db *gorm.DB) error {
-		tx := db.Begin()
-		for _, item := range items {
-			if err := tx.Save(item).Error; err != nil {
-				logrus.Warnf("DB.SaveMany Error Conn:%s Type:%s (%s)", r.dbKey, reflect.TypeOf(item).String(), err.Error())
-				tx.Rollback()
-				return err
-			}
-		}
-		tx.Commit()
-		return nil
-	})
-}
-
+// Update 更新列
 func (r *DatabaseRepo) Update(model interface{}, query string, params []interface{}, item ...interface{}) error {
 
 	r.put()
 	defer r.pop()
 
-	db, err := GetDB(r.dbKey)
+	db, err := getDB(r.dbKey)
 	if err != nil {
-		return cerror.NewCodeError(cerror.DB_ERROR, err)
-
+		return err
 	}
+
 	defer db.Close()
 	db = db.Model(model).Where(query, params...)
-	if err := db.Update(item...).Error; err != nil {
-		logrus.Warnf("DB.Update Error Conn:%s Type:%s (%s)", r.dbKey, reflect.TypeOf(item).String(), err.Error())
-		return cerror.NewCodeError(cerror.DB_ERROR, err)
-
+	db = db.Update(item...)
+	if err := db.Error; err != nil {
+		return warpDBError(db, "DB.Update", fmt.Sprintf("DB.Update Error Conn:%s Type:%s", r.dbKey, reflect.TypeOf(item).String()))
 	}
 	return nil
+
 }
 
 func (r *DatabaseRepo) UpdateColumn(model interface{}, query string, params []interface{}, attrs ...interface{}) error {
@@ -96,15 +74,15 @@ func (r *DatabaseRepo) UpdateColumn(model interface{}, query string, params []in
 	r.put()
 	defer r.pop()
 
-	db, err := GetDB(r.dbKey)
+	db, err := getDB(r.dbKey)
 	if err != nil {
-		return cerror.NewCodeError(cerror.DB_ERROR, err)
-
+		return err
 	}
 	defer db.Close()
-	if err := db.Model(model).Where(query, params...).UpdateColumn(attrs...).Error; err != nil {
-		logrus.Warnf("DB.UpdateColumn Error Conn:%s Type:%s (%s)", r.dbKey, reflect.TypeOf(model).String(), err.Error())
-		return cerror.NewCodeError(cerror.DB_ERROR, err)
+
+	db = db.Model(model).Where(query, params...).UpdateColumn(attrs...)
+	if err := db.Error; err != nil {
+		return warpDBError(db, "DB.UpdateColumn", fmt.Sprintf("DB.UpdateColumn Error Conn:%s Type:%s", r.dbKey, reflect.TypeOf(model).String()))
 	}
 	return nil
 }
@@ -114,34 +92,38 @@ func (r *DatabaseRepo) Updates(model interface{}, query string, where []interfac
 	r.put()
 	defer r.pop()
 
-	db, err := GetDB(r.dbKey)
+	db, err := getDB(r.dbKey)
 	if err != nil {
-		return cerror.NewCodeError(cerror.DB_ERROR, err)
-
+		return err
 	}
 	defer db.Close()
-	if err := db.Model(model).Where(query, where...).Updates(item).Error; err != nil {
-		logrus.Warnf("DB.Updates Error Conn:%s Type:%s (%s)", r.dbKey, reflect.TypeOf(item).String(), err.Error())
-		return cerror.NewCodeError(cerror.DB_ERROR, err)
 
+	db = db.Model(model).Where(query, where...).Updates(item)
+
+	if err := db.Error; err != nil {
+		return warpDBError(db, "DB.Updates", fmt.Sprintf("DB.Updates Error Conn:%s Type:%s", r.dbKey, reflect.TypeOf(item).String()))
 	}
 	return nil
+
 }
 
-func (r *DatabaseRepo) Delete(item interface{}) error {
+func (r *DatabaseRepo) Delete(item interface{}, query string, params ...interface{}) error {
 
 	r.put()
 	defer r.pop()
 
-	db, err := GetDB(r.dbKey)
+	db, err := getDB(r.dbKey)
 	if err != nil {
-		return cerror.NewCodeError(cerror.DB_ERROR, err)
-
+		return err
 	}
 	defer db.Close()
-	if err := db.Delete(item).Error; err != nil {
-		logrus.Warnf("DB.Delete Error Conn:%s Type:%s (%s)", r.dbKey, reflect.TypeOf(item).String(), err.Error())
-		return cerror.NewCodeError(cerror.DB_ERROR, err)
+	query = strings.Trim(query, "")
+	if query != "" {
+		db = db.Where(query, params...)
+	}
+	db = db.Delete(item)
+	if err := db.Error; err != nil {
+		return warpDBError(db, "DB.Updates", fmt.Sprintf("DB.Delete Error Conn:%s Type:%s", r.dbKey, reflect.TypeOf(item).String()))
 	}
 	return nil
 }
@@ -155,197 +137,151 @@ type SearchOption struct {
 	TotalOut *int
 }
 
-func (r *DatabaseRepo) FirstEX(item interface{}, option SearchOption) *gorm.DB {
+func (r *DatabaseRepo) First(item interface{}, where string, params ...interface{}) error {
+
+	return r.FirstEX(item, SearchOption{
+		Limit:  1,
+		Offset: 0,
+		Where:  where,
+		Params: params,
+	})
+}
+func (r *DatabaseRepo) FirstEX(item interface{}, option SearchOption) error {
 	r.put()
 	defer r.pop()
-	db, err := GetDB(r.dbKey)
+	db, err := getDB(r.dbKey)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer db.Close()
+
+	option.Order = strings.Trim(option.Order, "")
 	if option.Order != "" {
 		db = db.Order(option.Order)
 	}
 	values := []interface{}{}
+	option.Where = strings.Trim(option.Where, "")
 	if option.Where != "" {
 		values = append(values, option.Where)
 		values = append(values, option.Params...)
 	}
 	db = db.Offset(option.Offset)
 	db = db.First(item, values...)
-	if db.Error != nil && !db.RecordNotFound() {
-		logrus.Warnf("DB.FirstEX Error Conn:%s Type:%s WHERE:%v (%s)", r.dbKey, reflect.TypeOf(item).String(), option.Where, db.Error.Error())
+
+	if db.Error != nil {
+		return warpDBError(db, "DB.First", fmt.Sprintf("DB.First Error Conn:%s Type:%s WHERE:%v", r.dbKey, reflect.TypeOf(item).String(), option.Where))
 	}
-	return db
+	return nil
 
 }
 
-func (r *DatabaseRepo) FindEx(item interface{}, option SearchOption) *gorm.DB {
+func (r *DatabaseRepo) Find(list interface{}, offset int, limit int, where string, order string, params ...interface{}) error {
+	return r.FindEX(list, SearchOption{
+		Limit:  offset,
+		Offset: limit,
+		Where:  where,
+		Params: params,
+		Order:  order,
+	})
+}
+
+func (r *DatabaseRepo) FindEX(list interface{}, option SearchOption) error {
 	r.put()
 	defer r.pop()
-	db, err := GetDB(r.dbKey)
+	db, err := getDB(r.dbKey)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer db.Close()
-	if option.Order != "" {
-		db = db.Order(option.Order)
-	}
+	option.Order = strings.Trim(option.Order, "")
+
 	values := []interface{}{}
+	option.Where = strings.Trim(option.Where, "")
 	if option.Where != "" {
 		values = append(values, option.Where)
 		values = append(values, option.Params...)
 	}
 
-	dbCount := db
-
-	if option.TotalOut != nil {
-		if err := dbCount.Model(item).Where(option.Where, option.Params...).Count(option.TotalOut).Error; err != nil {
-			logrus.Warnf("DB.FirstEX:Count Error Conn:%s Type:%s WHERE:%v (%s)", r.dbKey, reflect.TypeOf(item).String(), option.Where, err.Error())
-
-		}
-	}
-
 	if option.Order != "" {
 		db = db.Order(option.Order)
 	}
 
+	dbCount := db
+
+	if option.TotalOut != nil {
+		dbCount.Model(list).Where(option.Where, option.Params...).Count(option.TotalOut)
+	}
+
 	db = db.Offset(option.Offset)
-	if option.Limit > 0 || option.Limit == -1 {
+	if option.Limit > 0 {
 		db = db.Limit(option.Limit)
+	} else {
+		db = db.Limit(math.MaxInt32)
 	}
 
-	db = db.Find(item, values...)
-	if db.Error != nil && !db.RecordNotFound() {
-		logrus.Warnf("DB.FirstEX Error Conn:%s Type:%s WHERE:%v (%s)", r.dbKey, reflect.TypeOf(item).String(), option.Where, db.Error.Error())
-	}
-	return db
+	db = db.Find(list, values...)
 
-}
+	if db.Error != nil {
+		return warpDBError(db, "DB.Find", fmt.Sprintf("DB.Find Error Conn:%s Type:%s WHERE:%v", r.dbKey, reflect.TypeOf(list).String(), option.Where))
 
-func (r *DatabaseRepo) First(item interface{}, where ...interface{}) *gorm.DB {
-
-	r.put()
-	defer r.pop()
-
-	db, err := GetDB(r.dbKey)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-	db = db.First(item, where...)
-	if db.Error != nil && !db.RecordNotFound() {
-		logrus.Warnf("DB.First Error Conn:%s Type:%s WHERE:%v (%s)", r.dbKey, reflect.TypeOf(item).String(), where, db.Error.Error())
-	}
-	return db
-}
-
-func (r *DatabaseRepo) DeleteWhere(item interface{}, query string, params ...interface{}) error {
-
-	r.put()
-	defer r.pop()
-
-	db, err := GetDB(r.dbKey)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-	if err := db.Where(query, params...).Delete(item).Error; err != nil {
-		logrus.Warnf("DB.DeleteWhere Error Conn:%s Type:%s WHERE:%s (%s)", r.dbKey, reflect.TypeOf(item).String(), query, err.Error())
-		return cerror.NewCodeError(cerror.DB_ERROR, err)
 	}
 	return nil
+
 }
 
-func (r *DatabaseRepo) Find(offset int, limit int, list interface{}, where ...interface{}) error {
+func (r *DatabaseRepo) Count(item interface{}, query string, values ...interface{}) (int, *DbError) {
 
 	r.put()
 	defer r.pop()
 
-	db, err := GetDB(r.dbKey)
-	if err != nil {
-	}
-	defer db.Close()
-	db = db.Offset(offset)
-	if limit > 0 {
-		db = db.Limit(limit)
-	}
-	if err := db.Find(list, where...).Error; err != nil && !db.RecordNotFound() {
-		logrus.Warnf("DB.Find Error Conn:%s Type:%s WHERE:%v (%s)", r.dbKey, reflect.TypeOf(list).String(), where, err.Error())
-		return cerror.NewCodeError(cerror.DB_ERROR, err)
-	}
-	return nil
-}
-
-func (r *DatabaseRepo) FindC(offset int, limit int, list interface{}, where ...interface{}) (int, error) {
-
-	r.put()
-	defer r.pop()
-
-	db, err := GetDB(r.dbKey)
-	if err != nil {
-	}
-	defer db.Close()
-
-	db = db.Offset(offset)
-	if limit > 0 {
-		db = db.Limit(limit)
-	}
-	if err := db.Find(list, where...).Error; err != nil {
-		logrus.Warnf("DB.Find Error Conn:%s Type:%s WHERE:%v (%s)", r.dbKey, reflect.TypeOf(list).String(), where, err.Error())
-		return 0, cerror.NewCodeError(cerror.DB_ERROR, err)
-	}
-	return 0, nil
-}
-
-func (r *DatabaseRepo) Count(item interface{}, query string, values ...interface{}) (int, error) {
-
-	r.put()
-	defer r.pop()
-
-	db, err := GetDB(r.dbKey)
+	db, err := getDB(r.dbKey)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 	total := 0
-	if err := db.Model(item).Where(query, values...).Count(&total).Error; err != nil {
-		logrus.Warnf("DB.Count Error Conn:%s SQL:%s WHERE:%s... (%s)", r.dbKey, query, values, err.Error())
-		return -1, cerror.NewCodeError(cerror.DB_ERROR, err)
+
+	db = db.Model(item).Where(query, values...).Count(&total)
+	if err := db.Error; err != nil {
+		return -1, warpDBError(db, "DB.Count", fmt.Sprintf("DB.Count Error Conn:%s SQL:%s WHERE:%s... ", r.dbKey, query, values))
 	}
 	return total, nil
 }
 
-func (r *DatabaseRepo) Raw(sql string, values ...interface{}) error {
+func (r *DatabaseRepo) Exec(sql string, values ...interface{}) error {
 
 	r.put()
 	defer r.pop()
 
-	db, err := GetDB(r.dbKey)
+	db, err := getDB(r.dbKey)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer db.Close()
-	if err := db.Raw(sql, values...).Error; err != nil {
-		logrus.Warnf("DB.Raw Error Conn:%s SQL:%s WHERE:%s... (%s)", r.dbKey, sql, values, err.Error())
-		return cerror.NewCodeError(cerror.DB_ERROR, err)
+	db = db.Exec(sql, values...)
+	if db.Error != nil {
+		return warpDBError(db, "DB.Exec", fmt.Sprintf("DB.Exec Error Conn:%s SQL:%s WHERE:%s...", r.dbKey, sql, values))
 	}
 	return nil
 }
 
-type RowScanHandler func(*sql.Rows) error
+type RowScanHandler func(*gorm.DB, *sql.Rows) error
 type TrasnsationInvokeHandler func(db *gorm.DB) error
 
 func (r *DatabaseRepo) InvokeTransation(callback TrasnsationInvokeHandler) error {
 	r.put()
 	defer r.pop()
-	db, err := GetDB(r.dbKey)
+	db, err := getDB(r.dbKey)
 
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer db.Close()
-	return callback(db)
+	var e = callback(db)
+	if e != nil {
+		return warpDBError(db, "DB.InvokeTransation", e.Error())
+	}
+	return nil
 }
 
 func (r *DatabaseRepo) RawSelect(rawSQL string, rowScanCallback RowScanHandler, values ...interface{}) error {
@@ -353,77 +289,38 @@ func (r *DatabaseRepo) RawSelect(rawSQL string, rowScanCallback RowScanHandler, 
 	r.put()
 	defer r.pop()
 
-	db, err := GetDB(r.dbKey)
+	db, err2 := getDB(r.dbKey)
 
-	if err != nil {
-		panic(err)
+	if err2 != nil {
+		return err2
 	}
 
 	defer db.Close()
 	var rows *sql.Rows
+	var err error
+
 	if rows, err = db.Raw(rawSQL, values...).Rows(); err != nil {
-		logrus.Warnf("DB.RawSelect Error Conn:%s SQL:%s WHERE:%s... (%s)", r.dbKey, rawSQL, values, err.Error())
-		return cerror.NewCodeError(cerror.DB_ERROR, err)
+		return warpDBError(db, "DB.RawSelect", fmt.Sprintf("DB.RawSelect Error Conn:%s SQL:%s WHERE:%s...", r.dbKey, rawSQL, values))
 	}
 
 	defer rows.Close()
 	for rows.Next() {
 		if rowScanCallback != nil {
-			err = rowScanCallback(rows)
+			err = rowScanCallback(db, rows)
 			if err != nil {
-				return err
+				return warpDBError(db, "DB.RawSelect", err.Error())
+
 			}
 		}
 	}
 	return nil
 }
 
-func (r *DatabaseRepo) RawSelectScan(rawSQL string, callback func(*gorm.DB, *sql.Rows) error, values ...interface{}) error {
-
-	r.put()
-	defer r.pop()
-
-	db, err := GetDB(r.dbKey)
-
-	if err != nil {
-		panic(err)
-	}
-
-	defer db.Close()
-	var rows *sql.Rows
-	if rows, err = db.Raw(rawSQL, values...).Rows(); err != nil {
-		logrus.Warnf("DB.RawSelectScan Error Conn:%s SQL:%s WHERE:%s... (%s)", r.dbKey, rawSQL, values, err.Error())
-		return cerror.NewCodeError(cerror.DB_ERROR, err)
-	}
-
-	defer rows.Close()
-	for rows.Next() {
-		if e := callback(db, rows); e != nil {
-			return e
-		}
-	}
-	return nil
-}
-
-func (r *DatabaseRepo) Exec(sql string, values ...interface{}) error {
-
-	r.put()
-	defer r.pop()
-	db, err := GetDB(r.dbKey)
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-	db = db.Exec(sql, values...)
-	return db.Error
-}
-
 func (r *DatabaseRepo) ExecuteScalar(rawSQL string, params []interface{}, values ...interface{}) error {
 
 	r.put()
 	defer r.pop()
-
-	db, err2 := GetDB(r.dbKey)
+	db, err2 := getDB(r.dbKey)
 
 	if err2 != nil {
 		return err2
@@ -433,7 +330,7 @@ func (r *DatabaseRepo) ExecuteScalar(rawSQL string, params []interface{}, values
 	var rows *sql.Rows
 	var err error
 	if rows, err = db.Raw(rawSQL, params...).Rows(); err != nil {
-		return err
+		return warpDBError(db, "DB.RawSelect", fmt.Sprintf("DB.ExecuteScalar Error Conn:%s SQL:%s WHERE:%s...", r.dbKey, rawSQL, params))
 	}
 
 	defer rows.Close()
